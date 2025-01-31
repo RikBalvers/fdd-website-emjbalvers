0c7ab90d27e02583f38f89b94dc3ec00b5e0f857
Rik Balvers <rikbalvers@outlook.com>
2025-01-31 04:03:06 +0100

    testen

---
+++ js/loadVideo.js
@@ -0,0 +1,3 @@
+function loadYouTubeVideo(container) {
+    container.innerHTML = '<a href="https://www.youtube.com/watch?v=jfKfPfyJRdk" class="video-overlay" target="_blank">Video niet beschikbaar</a>';
+}
